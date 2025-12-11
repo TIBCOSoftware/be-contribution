eb1bb604324d30814bba1ea77cf70ab99e9ca14c
Rohini Jadhav <rjadhav@tibco.com>
2025-12-11 17:53:15 +0530

    BE-34449 : Custom FTL Serializer for FTL Channel

--- catalog/ftl-catalog-fn/src/main/java/com/tibco/cep/functions/channel/ftl/MessageHelper.java
+++ catalog/ftl-catalog-fn/src/main/java/com/tibco/cep/functions/channel/ftl/MessageHelper.java
@@ -25,7 +25,7 @@ public class MessageHelper {
 
             },
             freturn = @com.tibco.be.model.functions.FunctionParamDescriptor(name = "", type = "boolean", desc = ""),
-            version = "5.2",
+            version = "6.3",
             see = "",
             mapper = @com.tibco.be.model.functions.BEMapper(),
             description = "Determine whether a field is set in a message.If it is not set, then getting the field value throws an exception.",
@@ -56,7 +56,7 @@ public class MessageHelper {
 
             },
             freturn = @com.tibco.be.model.functions.FunctionParamDescriptor(name = "", type = "String", desc = ""),
-            version = "5.2",
+            version = "6.3",
             see = "Get the value of a string field from a message.The string is valid only for the lifetime of the message. The string is part of the message object; the program must neither modify nor free it.",
             mapper = @com.tibco.be.model.functions.BEMapper(),
             description = "",
@@ -86,7 +86,7 @@ public class MessageHelper {
             @com.tibco.be.model.functions.FunctionParamDescriptor(name = "fieldname", type = "String", desc = "field name ")
         },
         freturn = @com.tibco.be.model.functions.FunctionParamDescriptor(name = "", type = "int", desc = ""),
-        version = "5.2",
+        version = "6.3",
         see = "",
         mapper = @com.tibco.be.model.functions.BEMapper(),
         description = "Get the type of a field within the message.",
@@ -117,7 +117,7 @@ public class MessageHelper {
 
             },
             freturn = @com.tibco.be.model.functions.FunctionParamDescriptor(name = "", type = "Object", desc = "opaque object"),
-            version = "5.2",
+            version = "6.3",
             see = "",
             mapper = @com.tibco.be.model.functions.BEMapper(),
             description = "Get the value of an opaque field from a message.This method copies the bytes from the message.",
@@ -149,7 +149,7 @@ public class MessageHelper {
 
             },
             freturn = @com.tibco.be.model.functions.FunctionParamDescriptor(name = "", type = "Object", desc = "message object"),
-            version = "5.2",
+            version = "6.3",
             see = "",
             mapper = @com.tibco.be.model.functions.BEMapper(),
             description = "Get the value of a message field from a message.This call deserializes the sub-message value, caches the result with the message object, and returns that cached sub-message. The sub-message is valid only for the lifetime of the parent message. Your program must not modify nor destroy the sub-message.Calling this method repeatedly returns the same cached sub-message; it does not repeat the deserialization.",
@@ -180,7 +180,7 @@ public class MessageHelper {
             @com.tibco.be.model.functions.FunctionParamDescriptor(name = "fieldname", type = "String", desc = "field name ")
         },
         freturn = @com.tibco.be.model.functions.FunctionParamDescriptor(name = "", type = "double", desc = ""),
-        version = "5.2",
+        version = "6.3",
         see = "",
         mapper = @com.tibco.be.model.functions.BEMapper(),
         description = "Get the value of a double floating-point field from a message.",
@@ -211,7 +211,7 @@ public class MessageHelper {
 
             },
             freturn = @com.tibco.be.model.functions.FunctionParamDescriptor(name = "", type = "long", desc = ""),
-            version = "5.2",
+            version = "6.3",
             see = "",
             mapper = @com.tibco.be.model.functions.BEMapper(),
             description = "Get the value of a long integer field from a message.",
@@ -241,7 +241,7 @@ public class MessageHelper {
         @com.tibco.be.model.functions.FunctionParamDescriptor(name = "fieldName", type = "String", desc = "field name ")
         },
         freturn = @com.tibco.be.model.functions.FunctionParamDescriptor(name = "", type = "void", desc = ""),
-        version = "5.2",
+        version = "6.3",
         see = "",
         mapper = @com.tibco.be.model.functions.BEMapper(),
         description = "Clear a field in a mutable message. Clearing a field clears the data from a field in the message object, and flags the field so a subsequent send call does not transmit it.",
@@ -262,7 +262,7 @@ public class MessageHelper {
         @com.tibco.be.model.functions.FunctionParamDescriptor(name = "message", type = "Object", desc = "message Object")
         },
         freturn = @com.tibco.be.model.functions.FunctionParamDescriptor(name = "", type = "void", desc = ""),
-        version = "5.2",
+        version = "6.3",
         see = "",
         mapper = @com.tibco.be.model.functions.BEMapper(),
         description = "Clear all fields in a mutable message. After clearing all fields, you can re-use the message. The message format does not change. This call is more efficient than creating a new empty message of the same format.",
@@ -286,7 +286,7 @@ public class MessageHelper {
 
             },
             freturn = @com.tibco.be.model.functions.FunctionParamDescriptor(name = "", type = "void", desc = ""),
-            version = "5.2",
+            version = "6.3",
             see = "",
             mapper = @com.tibco.be.model.functions.BEMapper(),
             description = "Set a string field in a mutable message.This method copies the string value into the message.",
@@ -323,7 +323,7 @@ public class MessageHelper {
 
             },
             freturn = @com.tibco.be.model.functions.FunctionParamDescriptor(name = "", type = "void", desc = ""),
-            version = "5.2",
+            version = "6.3",
             see = "",
             mapper = @com.tibco.be.model.functions.BEMapper(),
             description = "Set a sub-message field in a mutable message.This call copies the sub-message data into the enclosing message field, but does not create a new Java message object. Programs may safely destroy the msg argument after this call returns.Do not set a message as a sub-message of itself (at any level of nesting).",
@@ -361,7 +361,7 @@ public class MessageHelper {
 
             },
             freturn = @com.tibco.be.model.functions.FunctionParamDescriptor(name = "", type = "void", desc = ""),
-            version = "5.2",
+            version = "6.3",
             see = "",
             mapper = @com.tibco.be.model.functions.BEMapper(),
             description = "Set an opaque (byte-array) field in a mutable message.This method copies the entire byte-array into the field.",
@@ -396,7 +396,7 @@ public class MessageHelper {
             @com.tibco.be.model.functions.FunctionParamDescriptor(name = "message", type = "Object", desc = "message object")
         },
         freturn = @com.tibco.be.model.functions.FunctionParamDescriptor(name = "", type = "void", desc = ""),
-        version = "5.2",
+        version = "6.3",
         see = "",
         mapper = @com.tibco.be.model.functions.BEMapper(),
         description = "Destroy a message object. A program may destroy only mutable messages - that is, those messages that the program creates - for example, using Realm.createMessage of mutableCopy(). Inbound messages in listener callback methods belong to the FTL library, programs must not destroy them. Do not destroy a message if the program needs data that the message owns - for example, a string (from getString), an opaque pointer (from getOpaque), a sub-message (from getMessage), or an inbox (from getInbox). Destroying a message frees all resources associated with it.",
